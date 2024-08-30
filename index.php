d8f81720e26df8893f752f00f3325176fb5e627e
josperla <78237563+josperla@users.noreply.github.com>
2024-08-29 18:23:43 -0600

    Practica 2

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-6 m-auto">
+                <form action="procesar.php" method="post">
+                    <div class="card">
+                        <div class="card-header">
+                            Conversor de medidas
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                            <div class="col-12 mb-3">
+                                    <label for="from">De</label>
+                                    <select name="from" id="from" class="form-control">
+                                        <option value="km">Kilómetro</option>
+                                        <option value="hm">Hectómetro</option>
+                                        <option value="dam">Decámetro</option>
+                                        <option value="m">Metro</option>
+                                        <option value="dm">Decímetro</option>
+                                        <option value="cm">Centímetro</option>
+                                        <option value="mm">Milímetro</option>
+                                    </select>
+                                </div>
+
+                                <div class="col-12 mb-3">
+                                    <label for="cantidad">Cantidad</label>
+                                    <input type="number" name="cantidad" id="cantidad" min="0" class="form-control" 
+                                    placeholder="Ingresa la cantidad a convertir"required>
+                                </div>
+
+                                <div class="col-12 mb-3">
+                                    <label for="to">A</label>
+                                    <select name="to" id="to" class="form-control">
+                                        <option value="km">Kilómetro</option>
+                                        <option value="hm">Hectómetro</option>
+                                        <option value="dam">Decámetro</option>
+                                        <option value="m">Metro</option>
+                                        <option value="dm">Decímetro</option>
+                                        <option value="cm">Centímetro</option>
+                                        <option value="mm">Milímetro</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Procesar</button>
+                            <button type="reset" class="btn btn-secondary">Cancelar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
